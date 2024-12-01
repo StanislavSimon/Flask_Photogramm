0990905a1a50198273b6674985bf28681d219dea
StanislavSimon <simonenkostanislav@rambler.ru>
2024-12-01 19:29:14 +0300

    Add files

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
+from models import User
+from database import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SECRET_KEY'] = 'your_secret_key'  # Установите свой секретный ключ
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('register'))  # Перенаправление на страницу регистрации
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            flash('Имя пользователя уже занято')
+            return redirect(url_for('register'))
+
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Регистрация успешна! Пожалуйста, войдите.')
+        return redirect(url_for('login'))  # Перенаправление на страницу входа после регистрации
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.verify_password(password):
+            flash('Вход успешен!')
+            return redirect(url_for('index_page'))  # Перенаправление на главную страницу после входа
+
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index_page'))  # Перенаправление на главную страницу после загрузки файла
+
+    return render_template('upload.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/index')
+def index_page():
+    uploaded_files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', uploaded_files=uploaded_files)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
